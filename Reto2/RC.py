855b104beb5ec268509321aff564be5f6b63f1e5
evertacostadevPC <evertacostadev@gmail.com>
2022-09-08 23:45:53 -0500

    prefinal jueves 8

--- Reto2/RC.py
+++ Reto2/RC.py
@@ -11,6 +11,7 @@ def read_adf(file):
     return pd.read_csv(file, sep="\t", names=['t', 'y1', 'y2'], usecols=[0, 1, 2], index_col=0, skiprows=2)
 
 
+
 # Fuente doble exponencial
 def double_exp(time, p):
     t = time
